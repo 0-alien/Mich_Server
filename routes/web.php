83381a9803d61e6c6b9542274fc16e60a05932bf
g-tskhondia <giorgi.tskhondia@singular.uk>
2017-03-06 14:06:01 +0400

    Add pusher

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 
+Route::get('push', function () {
+
+  $options = ['cluster' => 'eu', 'encrypted' => true];
+  $pusher = new Pusher(env('PUSHER_KEY'), env('PUSHER_SECRET'), env('PUSHER_APP_ID'), $options);
+  $pusher->trigger('my-channel', 'my-event', 'hello mich');
+
+});
+
+
+
 Route::get('/', function () {
-  return '<pre>MICH_SERVER</pre>';
+  return view('pusher');
 });
