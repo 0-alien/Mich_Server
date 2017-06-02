b405382ec4a4553dc973a395459cf0fc55723842
0-alien <alien@post.com>
2017-06-02 16:04:22 +0400

    Add notification id

--- app/MS/Models/Notification.php
+++ app/MS/Models/Notification.php
@@ -19,7 +19,7 @@ class Notification extends Model {
 
   public function send() {
     $badge = Notification::where('userid', $this->userid)->where('status', 0)->count();
-    $data = ['type' => $this->type, 'id' => $this->itemid];
+    $data = ['type' => $this->type, 'id' => $this->itemid, 'notificationid' => $this->id];
     if ($this->type === 2 || $this->type === 3) {
       $data['commentid'] = $data['id'];
       unset($data['id']);
