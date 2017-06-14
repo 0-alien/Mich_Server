0957bbb987e6534b6eed4c71de9e6d1b4d7d4d0a
Giorgi <giorgi@Giorgis-MacBook-Pro.local>
2017-06-15 00:55:06 +0400

    Add notifications to cron

--- app/Http/Controllers/Cron/CronController.php
+++ app/Http/Controllers/Cron/CronController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Cron;
 
 use App\Http\Controllers\BaseController;
 use App\MS\Models\Battle\Battle;
+use App\MS\Models\Notification;
 use Carbon\Carbon;
 
 class CronController extends BaseController {
@@ -16,6 +17,15 @@ class CronController extends BaseController {
       if ($now->diffInMinutes(Carbon::parse($battle->created_at))) {
         $battle->status = 2;
         $battle->save();
+
+        $notification = new Notification();
+        $notification->type = 7;
+        $notification->battleid = $battle->id;
+        $notification->message = 'invitation timed out';
+        $notification->avatar = url('/api/media/display/' . $battle->guestCredential->user->avatar);
+        $notification->userid = $battle->host;
+        $notification->save();
+        $notification->send();
       }
     }
 
@@ -24,6 +34,24 @@ class CronController extends BaseController {
       if ($now->diffInHours(Carbon::parse($battle->created_at))) {
         $battle->status = 3;
         $battle->save();
+
+        $notification = new Notification();
+        $notification->type = 8;
+        $notification->battleid = $battle->id;
+        $notification->message = 'battle timed out';
+        $notification->avatar = url('/api/media/display/' . $battle->guestCredential->user->avatar);
+        $notification->userid = $battle->host;
+        $notification->save();
+        $notification->send();
+
+        $notification = new Notification();
+        $notification->type = 8;
+        $notification->battleid = $battle->id;
+        $notification->message = 'battle timed out';
+        $notification->avatar = url('/api/media/display/' . $battle->hostCredential->user->avatar);
+        $notification->userid = $battle->guest;
+        $notification->save();
+        $notification->send();
       }
     }
   }
