761367dd80b9591d89ddf5c3c563a3e0ea091d6a
0-alien <alien@post.com>
2017-06-22 13:35:43 +0400

    fix

--- app/MS/Models/Battle/Battle.php
+++ app/MS/Models/Battle/Battle.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Battle extends Model {
 
-  protected $fillable = ['host', 'guest', 'hostvotes', 'guestvotes', 'status'];
+  protected $fillable = ['host', 'guest', 'status'];
 
 
 
